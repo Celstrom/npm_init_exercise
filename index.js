75b1f474fdf9de69787d9f823b62f52d5db6949b
Celstrom <103147561+Celstrom@users.noreply.github.com>
2022-10-16 21:32:20 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+{
+  "name": "npm_init_exercise",
+  "version": "1.0.0",
+  "description": "npm init exercise",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/Celstrom/npm_init_exercise.git"
+  },
+  "keywords": [
+    "npm",
+    "init"
+  ],
+  "author": "Colton Elstrom",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/Celstrom/npm_init_exercise/issues"
+  },
+  "homepage": "https://github.com/Celstrom/npm_init_exercise#readme"
+}
